5a4e961a5a52a686f9fadaa0fe2b1d2fc860d24b
dgramaciotti <dgramaciotti@gmail.com>
2025-12-26 11:04:55 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <button onClick={() => console.log('hello there')}>click</button>
+      hello world
+    </main>
+  )
+}
+
+export default App;
